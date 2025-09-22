210179a5e800e2c73dde743df90a85fdc68a990e
JinYoungBae-kw <bjo0302@naver.com>
2025-09-23 04:13:08 +0900

    login_design

--- app/src/main/java/com/android/example/myapplication/AppConfig.java
+++ app/src/main/java/com/android/example/myapplication/AppConfig.java
@@ -3,7 +3,7 @@ package com.android.example.myapplication;
 public class AppConfig {
     // Centralized URL configuration
     // Change this URL here to update it across the entire application
-    public static final String BASE_URL = "http://192.168.137.121:8000";
+    public static final String BASE_URL = "http://192.168.137.1:8000";
 
     // Alternative URLs for different environments
     // Uncomment the one you want to use:
